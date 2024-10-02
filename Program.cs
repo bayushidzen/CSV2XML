984c216238b311526aa1ee98bdc2f8cafc963b43
bayushidzen <zhek_wh@mail.ru>
2024-10-02 21:08:04 +0300

    Определение путей для работы с файлами, выборка csv, чтение их и создание резервной копии.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string logpath = @"\logs";
+            string logpathxml = @"\output_to_bd\";
+            string logpathdone = @"\done\done_";
+
+            string[] dirs = Directory.GetFiles(logpath, "*.csv");
+            foreach (string dir in dirs)
+            {
+                Console.WriteLine(dir);
+                string[] source = File.ReadAllLines(dir);
+                string filename = Path.GetFileName(dir) + ".xml";
+                File.Copy(dir, logpathdone + Path.GetFileName(dir), true);
+            }
         }
     }
 }
